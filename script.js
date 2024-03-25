9c27c67477c9d047f676afafe8419211bcd32678
JeanCarloGuzmanC <111938847+JeanCarloGuzmanC@users.noreply.github.com>
2024-03-25 15:16:09 -0500

    creación inicial de la apariencia

---
+++ script.js
@@ -0,0 +1,10 @@
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").classList.remove("hidden");
+  } else {
+    document.getElementById("navbar").classList.add("hidden");
+  }
+  prevScrollpos = currentScrollPos;
+}
